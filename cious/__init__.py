e765d7132819f823fd95124dcb819cf4977bf547
bfcropp <bfcropp@gmail.com>
2020-02-21 18:05:43 -0500

    minor cosmetic changes and updates

--- cious/__init__.py
+++ cious/__init__.py
@@ -7,7 +7,7 @@ import os
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/static')
 
     app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/magpie/cious/cious/db.sqlite'
